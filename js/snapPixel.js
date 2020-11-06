24c0df138c892795626e45a74f13b2698e3aa304
Santiago Barrios H <santiagobarrios08@gmail.com>
2020-11-06 02:16:22 +0100

    this update. contain multiple buttons array pulling JSON data

--- js/snapPixel.js
+++ js/snapPixel.js
@@ -11,3 +11,21 @@ snaptr('init', '0b8d5865-1f54-4a8f-bb50-441971bd8919', {
 });
 
 snaptr('track', 'PAGE_VIEW');
+//SNAP pixel: the code below will track add cart buttons and catching up the item id as well
+window.addEventListener('DOMContentLoaded', SnapPixel);
+
+if (ShopifyAnalytics.meta.page.pageType.includes('home') || ShopifyAnalytics.meta.page.pageType.includes('collection')) {
+  const num = document.querySelectorAll('button[name="add"]');
+  num.forEach(conItems);
+
+  function conItems(item, index, arr) {
+    num[index].addEventListener('click', () => {
+      sessionStorage.itemId = JSON.parse(document.querySelectorAll('div[class="hide"')[index].innerText)[0].id;
+      sessionStorage.itemPrice = JSON.parse(document.querySelectorAll('div[class="hide"')[index].innerText)[0].price / 100;
+      snaptr('track', 'ADD_CART', {
+        'item_ids': sessionStorage.itemId,
+        'price': sessionStorage.itemPrice
+      });
+    })
+  }
+}
